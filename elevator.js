5c9e47f51925a5d454eedb631bf4585045e95215
Wen Ou <wenou7@gmail.com>
2019-02-24 23:17:30 -0500

    Pick up passengers from other floors

--- elevator.js
+++ elevator.js
@@ -1,16 +1,41 @@
 {
   init: function(elevators, floors) {
-      let elevator = elevators[0]
-
-      elevators.forEach(elevator => {
-        
+    let up_pressed = floors.map(() => false)
+    let down_pressed = floors.map(() => false)
+    
+    floors.forEach(floor => {
+      floor.on("up_button_pressed", () => {
+        up_pressed[floor.floorNum()] = true
       })
-
-      elevator.on("idle", function() {
-        for (let floor = 0; floor < floors.length; floor++) {
+      floor.on("down_button_pressed", () => {
+        down_pressed[floor.floorNum()] = true
+      })
+    })
+    elevators.forEach(elevator => {
+      elevator.on("passing_floor", (floorNum, direction) => {
+        if (elevator.getPressedFloors().includes(floorNum)){
+          elevator.goToFloor(floorNum, true)
+        }
+      })
+      elevator.on("idle", () => {
+        if (elevator.getPressedFloors().length) {
+          elevator.goToFloor(elevator.getPressedFloors()[0])
+        } else {
+          let floor = up_pressed.indexOf(true) 
+          if (floor < 0) {
+            floor = down_pressed.indexOf(true)
+          }
+          if (floor < 0){
+            floor = 0
+          }
           elevator.goToFloor(floor)
         }
       })
+      elevator.on("stopped_at_floor", floorNum => {
+        up_pressed[floorNum] = false
+        down_pressed[floorNum] = false
+      })
+    })
   },
   update: function(dt, elevators, floors) {
   }
